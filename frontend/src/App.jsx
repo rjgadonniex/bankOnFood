4408db4346cddf2d818b1dd9c797627db029d49f
Carlo Fraley <carlo.fraley@ufl.edu>
2026-02-26 16:23:23 -0500

    Add React frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
+      <h1 style={{ color: '#005b96' }}>bankOnfood</h1>
+      <p style={{ color: '#4CAF50' }}>add stuff</p>
+    </div>
+  );
+}
