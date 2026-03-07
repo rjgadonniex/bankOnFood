9b702c3709ea330af514d98971537ffd3cd96279
Leo Jiang <leojiang99999@gmail.com>
2026-03-07 17:49:05 -0500

    home page navbar

---
+++ frontend/components/HomeNav.jsx
@@ -0,0 +1,33 @@
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import { ForkKnife } from "react-bootstrap-icons";
+function HomeNav() {
+  return (
+    <Navbar bg="white" expand="md" className="py-2 shadow-sm">
+      <Container fluid>
+        <Navbar.Brand href="/#" className="fw-bold fs-4 d-flex align-items-center">
+          <ForkKnife className="text-primary me-2" />
+          <span class="text-primary">bank</span>
+          <span class="text-success">On</span>
+          <span class="text-primary">Food</span>
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="navbar" className="border-0 shadow-none" />
+        <Navbar.Collapse id="navbar">
+          <Nav className="ms-auto align-items-center gap-2">
+            <Nav.Link href="#problem" className="text-dark">
+              The Problem
+            </Nav.Link>
+            <Nav.Link href="#solution" className="text-dark">
+              Our Solution
+            </Nav.Link>
+            <Button as="a" href="/login" className="btn btn-primary rounded-pill px-4 py-2">
+              Get Started
+            </Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default HomeNav;
--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,30 +1,11 @@
-import { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import HomeNav from "../components/HomeNav";
 
 export default function Home() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:5000/posts")
-      .then((res) => res.json())
-      .then((data) => setPosts(data))
-      .catch((err) => console.error(err));
-  }, []);
-
   return (
-    <div style={{ textAlign: "center", marginTop: "50px", fontFamily: "sans-serif" }}>
-      <h1 style={{ color: "#005b96" }}>bankOnfood</h1>
-      <p style={{ color: "#4CAF50" }}>add stuff</p>
-
-      {posts.length === 0 ? (
-        <p>None</p>
-      ) : (
-        posts.map((p) => (
-          <div key={p._id}>
-            <h3>{p.title}</h3>
-            <p>{p.content}</p>
-          </div>
-        ))
-      )}
-    </div>
+    <>
+      <HomeNav />
+      <div>Home</div>
+    </>
   );
 }
