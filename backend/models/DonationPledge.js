3eda2847775bc3b44298217c581fd77ada198201
adrianp-7 <aipavila7@gmail.com>
2026-03-31 22:40:09 -0400

    added a unit field to the Item and DonationPledge schema

--- backend/models/DonationPledge.js
+++ backend/models/DonationPledge.js
@@ -6,24 +6,25 @@ const DonationPledgeSchema = mongoose.Schema({
         ref: 'User',
         required: true
     },
-
     item: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Item",
         required: true
     },
-
     quantity: {
         type: Number,
         default: 1
     },
-
+    unit: {
+        type: String,
+        enum: ['lbs', 'cans', 'boxes', 'bags', 'units'],
+        default: 'units'
+    },
     status: {
         type: String,
         enum: ['pending', 'confirmed', 'received'],
         default: 'pending'
     },
-
     datePledged: {
         type: Date,
         default: Date.now
